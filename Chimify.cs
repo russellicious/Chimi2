a3f2e6c4b073e020f5c57d538d85930cdf10d84b
russellicious <russellsharp80@gmail.com>
2021-08-14 19:13:29 -0500

    Add project files.

---
+++ Chimify.cs
@@ -0,0 +1,28 @@
+﻿
+
+public interface IChimify {
+    object One { get; set; }
+
+    object Two { get; set; }
+
+    void Chimi (out object One, out object Two);
+}
+
+public class Chimify : IChimify {
+    private Chimify (object one, object two) {
+        one = null;
+        two = null;
+    }
+
+    public object One { get; set; }
+
+    public object Two { get; set; }
+
+    public void Chimi (out object One, out object Two) {
+        throw new System.NotImplementedException();
+    }
+
+    public static IChimify Factory (object one, object two) {
+        return new Chimify(one, two);
+    }
+}
